2042b5cd100622b43e1f2ed808d8df812ee7d93a
Hassadee Pimsuwan <h.pimsuwan@gmail.com>
2019-06-21 17:16:15 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+require('./vendor/TrackballControl');
+
+AFRAME.registerComponent('trackball-controls', {
+	dependencies: ['camera'],
+
+	schema: {
+
+	},
+
+	init: function() {
+
+	},
+
+	undate: function() {
+		
+	}
+});
